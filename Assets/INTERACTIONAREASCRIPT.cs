b6007d6a3262995ac3d03e77f0be699eb7613f59
48592475 <144272436+48592475@users.noreply.github.com>
2024-06-11 15:42:56 -0300

    Initial commit

---
+++ Assets/INTERACTIONAREASCRIPT.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class INTERACTIONAREASCRIPT : MonoBehaviour
+{
+    public Text puntosTexto;
+    public int Puntos = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        puntosTexto.text = Puntos.ToString();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        PickUpScript pickUp;
+        pickUp = other.GetComponent<PickUpScript>();
+        Puntos += pickUp.Puntos1;
+        if (pickUp.Comida == true)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
